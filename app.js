f89adba4ddafe57351caa6187e8bc21bfc82f7ed
SuprajaSana <sanasupraja2727@gmail.com>
2023-05-01 23:44:30 +0530

    routes

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const http = require("http");
 
-const server = http.createServer((re, res) => {
-    console.log("Supraja");
+const server = http.createServer((req, res) => {
+    const url=req.url
+    if (url === "/") {
+        res.write("<h1>Welcome to my node js project</h1>");
+        return res.end();
+    }
+     if (url === "/home") {
+       res.write("<h1>Welcome home page</h1>");
+       return res.end();
+     }
+     if (url === "/about") {
+       res.write("<h1>Welcome to About us page</h1>");
+       return res.end();
+     }
+     if (url === "/node") {
+       res.write("<h1>Welcome to my node js project</h1>");
+       return res.end();
+     }
+   
 })
 
 server.listen(4000)
