7c66556fd87bae2d67110890ab5dd886c14f5a01
SuprajaSana <sanasupraja2727@gmail.com>
2023-05-03 22:27:44 +0530

    writing and reading files

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    const url=req.url
+    const url = req.url;
+    const method = req.method;
     if (url === "/") {
-        res.write("<h1>Welcome to my node js project</h1>");
-        return res.end();
+        fs.readFile("message.txt", { encoding: "utf-8" }, (err,data) => {
+            if (err) {
+                console.log(err)
+            }
+             res.write("<html>");
+             res.write("<head><title>Enter meassage</title></head>");
+             res.write(`<body>${data}</body>`);
+             res.write(
+               "<form action='/message' method='POST'><input type='text' name='message'><button type='submit'>SEND</button></form>"
+            );
+            res.write("</html>");
+            return res.end();
+        })
+    }
+    if (url === "/message" && method === "POST") {
+
+        const body = [];
+        req.on("data", (chunk) => {
+            body.push(chunk);
+        })
+
+        return req.on("end", () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+            fs.writeFile("message.txt", message, (err) => {
+                 res.statusCode = 302;
+                 res.setHeader("Location", "/");
+                 return res.end();
+            });
+        })
     }
      if (url === "/home") {
        res.write("<h1>Welcome home page</h1>");
