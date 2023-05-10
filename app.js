4336d8c807623a6240b56b2a84f8856df47437d3
SuprajaSana <sanasupraja2727@gmail.com>
2023-05-10 22:24:23 +0530

    using body parser

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const http = require("http");
+const bodyParser=require("body-parser")
+
 const express=require("express")
 
 const app = express()
 
-app.use((req, res, next) => {
+app.use(bodyParser.urlencoded({encoded:false}))
+
+app.use("/add-product",(req, res, next) => {
     console.log("In the middleware");
-    next();
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>')
 })
-app.use((req, res, next) => {
+app.use("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/")
+});
+
+app.use("/",(req, res, next) => {
     console.log("In the another middleware");
     res.send("<h1>hello to node js</h1>")
 })
