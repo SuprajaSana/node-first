51de7c166b310b9422822ad1855d0eaedc53232f
SuprajaSana <sanasupraja2727@gmail.com>
2023-05-11 21:33:55 +0530

    separate route files

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-const http = require("http");
-const bodyParser=require("body-parser")
+const express = require("express");
 
-const express=require("express")
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({encoded:false}))
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use("/add-product",(req, res, next) => {
-    console.log("In the middleware");
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>')
-})
-app.use("/product", (req, res, next) => {
-    console.log(req.body);
-    res.redirect("/")
+app.use(bodyParser.urlencoded({ encoded: false }));
+
+app.use("/admin", adminRoutes);
+app.use("/shop",shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
-app.use("/",(req, res, next) => {
-    console.log("In the another middleware");
-    res.send("<h1>hello to node js</h1>")
-})
-
-app.listen(4000)
+app.listen(4000);
